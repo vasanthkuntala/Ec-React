273aa8f3e7f5a3a9b8bf29f5d9630d5dd3717500
vasanthkuntala <kuntalavasanthkumar@gmail.com>
2025-03-22 10:46:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,  // You can change this if needed
-    allowedHosts: ['https://ec-react-2bfq.onrender.com'],  // Add your Render domain
+   preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+    allowedHosts: "all"
   }
 });
